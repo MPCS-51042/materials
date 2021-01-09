fd652cb4043b0c4d97316bdc1deaf185b5edfdca
Chelsea Troy <chelsea.dommert@gmail.com>
2021-01-09 13:15:39 -0600

    Add income tax homework problems to ch3

---
+++ ch3/problem_5.py
@@ -0,0 +1,15 @@
+from problem_4 import taxes_owed
+import csv
+
+def total_impact(strategy):
+    '''
+        This function takes in a tax strategy as a string and
+        returns a tuple of tax_revenue and poverty_burden.
+
+        Inputs:
+            tax strategy (string)
+
+        Output:
+            tax_revenue and poverty_burden (tuple, float and integer)
+        '''
+    pass
---
+++ ch3/problem_6.py
@@ -0,0 +1,9 @@
+from problem_4 import taxes_owed
+
+def inflection_point():
+    '''
+        This function returns a float representing the income at which
+        Townsville's graduated income tax would be higher than a flat
+        5% income tax.
+    '''
+    pass
