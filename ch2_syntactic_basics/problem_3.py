d931931d760732e780e4e900ae8cd39b8b28056a
jmmunoz5 <jmunoz2@chicagobooth.edu>
2022-01-11 12:57:52 -0300

    save 2

--- ch2_syntactic_basics/problem_3.py
+++ ch2_syntactic_basics/problem_3.py
@@ -1,3 +1,9 @@
+#string_1 = "1, 2, 20,22, 44, 99"
+#string_2 = "3,5, 22, 100, 44, 2"
+
+def removeSpaces(string):
+    string = string.replace(' ','')
+    return string
 
 def find_twos(string_1, string_2):
     '''
@@ -13,6 +19,24 @@ def find_twos(string_1, string_2):
         Output:
             A list of integers, where the list contents is described by above. The returned list must not contain duplicates.
         '''
-    return []
+    string_1_no_space = removeSpaces(string_1)
+    list_1 = string_1_no_space.split(',')
+    string_2_no_space = removeSpaces(string_2)
+    list_2 = string_2_no_space.split(',')
+    
+    req_value = '2'
 
-#hi
+    output_list = []
+
+    for num in list_1:
+        if num in list_2 and req_value in num:
+            output_list.append(int(num))
+    
+    unique_list = []   
+    for item in output_list:
+        if item not in unique_list:
+            unique_list.append(item)
+
+    return unique_list
+    
+#print(find_twos(string_1,string_2))
--- ch2_syntactic_basics/problem_4.py
+++ ch2_syntactic_basics/problem_4.py
@@ -11,3 +11,28 @@ def expand_letter_ranges(string_of_letters):
             a list of the letters and expanded ranges in alphabetical order.
         '''
     pass
+
+
+
+string_1 = "1, 2, 20,22, 44, 99"
+string_2 = "3,5, 22, 100, 44, 2"
+
+def removeSpaces(string):
+    string = string.replace(' ','')
+    return string
+
+string_1_no_space = removeSpaces(string_1)
+
+list_1 = string_1_no_space.split(',')
+
+
+list_2 = string_2.split(',')
+req_value = '2'
+output_list = []
+
+for num in list_1:
+    if req_value in num:
+        print(int(num))
+
+print(list_1)
+print(list_2)
