246c52a02f0ba82a9ad76e648363f5455378eb79
Shubham Chandra <49321748+ShubhamMChandra@users.noreply.github.com>
2024-01-05 18:51:15 -0600

    Working through problem 4

--- ch2_syntactic_basics/problem_4.py
+++ ch2_syntactic_basics/problem_4.py
@@ -10,4 +10,26 @@ def expand_letter_ranges(string_of_letters):
         Output:
             a list of the letters and expanded ranges in alphabetical order.
         '''
-    pass
+    #take the string, seperate into a list by commas.
+    string_list = [value for value in string_of_letters.split(",")]
+    expanded_letters = set()
+    
+    for value in string_list:
+        if "-" in value:
+            start, end = value.split("-")
+            start_value = ord(start.upper())
+            end_value = ord(end.upper())
+            while start_value <= end_value:
+                expanded_letters.add(chr(start_value))
+                start_value +=1
+        else:
+            expanded_letters.add(value.upper())
+    expanded_letters_list = sorted(list(expanded_letters_set))
+    return expanded_letters
+            
+
+    # intiate new empty list numbered_list
+    # for each part of the list, if there's a hyphen, then what should I do?
+    # for items in list where there's no hyphen, convert to lowercase then convert to number with ord() and append to numbered_list.
+    # sort numbered list
+    #
