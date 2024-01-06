bcdbab0fdde7f8d291b35ca81535de067390f3db
Shubham Chandra <49321748+ShubhamMChandra@users.noreply.github.com>
2024-01-05 20:36:18 -0600

    started p7 part 1

--- ch2_syntactic_basics/problem_7.py
+++ ch2_syntactic_basics/problem_7.py
@@ -8,7 +8,30 @@ def find_minimum(sequence, comparator=None):
 
     :return: A tuple containing the smallest item and the index at which it occurs
     '''
-    pass
+    # assumes sequence is a list of integers as per examples given. 
+    error = "Invalid Sequence"
+    decreasing = False
+    increasing = False 
+
+    for i in range(1,len(sequence)-1):
+        if sequence[i] < sequence[i-1]:
+            if increasing == True:
+                return error
+            decreasing = True
+        elif sequence[i] == sequence[i-1]:
+            return error 
+        elif sequence[i] > sequence[i-1] and sequence[i-1] < sequence[i-2]:
+            increasing = True
+
+    if not (decreasing and increasing):  #doesnt make a v shape
+        return error
+
+    #now that we have confirmed valid, find min
+
+    for i in range(1, len(sequence) - 1):
+        if sequence[i] < sequence[i-1] and sequence[i] < sequence[i+1]:
+            return (sequence[i], i)
+
 
 class TrainingAttempt():
     def __init__(self, id, weights, error):
