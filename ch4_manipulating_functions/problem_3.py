30ec909a3435018b873742925a8017ac6bea9101
Chelsea Troy <chelsea.dommert@gmail.com>
2025-01-08 11:14:43 -0600

    Remind students not to import iterable

--- ch4_manipulating_functions/problem_3.py
+++ ch4_manipulating_functions/problem_3.py
@@ -1,2 +1,7 @@
+# As a reminder, do not import Iterable here. 
+# You don't need that library to check if something is iterable.
+# You know which data structures are iterable :) 
+# (hint: you can use isinstance(expression, type) to identify the type of an expression)
+
 def maketuple():
     pass
